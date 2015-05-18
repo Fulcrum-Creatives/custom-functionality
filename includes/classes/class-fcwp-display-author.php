a94352eb7c9b8f627980e8de1ad1afa9c6b8e5e5
jawittdesigns <jawittdesigns@gmail.com>
2015-05-18 12:52:27 -0400

    Updated Documentation

--- includes/classes/class-fcwp-display-author.php
+++ includes/classes/class-fcwp-display-author.php
@@ -17,6 +17,7 @@ if( !class_exists( 'FCWP_Display_Author' ) ) {
 		 * Initialize the class
 		 *
 		 * @since 0.0.1
+		 * @param array $args the class arguments
 		 */
 		public function __construct( $args = array() ) {
 			$this->args = $args;
