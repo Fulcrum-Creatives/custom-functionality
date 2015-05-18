49057b25bdf88a5b48ea73ae1337c7005dd32f26
jawittdesigns <jawittdesigns@gmail.com>
2015-05-18 09:48:15 -0400

    Changed variable name

--- includes/classes/class-fcwp-published-date.php
+++ includes/classes/class-fcwp-published-date.php
@@ -88,27 +88,27 @@ if( !class_exists( 'FCPW_Published_Date' ) ) {
 			$modified_container_attr   = ( !empty( $this->args['modified_container_attr'] ) ? $this->args['modified_container_attr'] : "" );
 			$published_text            = ( !empty( $this->args['published_text'] ) ? $this->args['published_text'] : "" );
 			$modified_text             = ( !empty( $this->args['modified_text'] ) ? $this->args['modified_text'] : "" );
-			$time_string               = '';
+			$string               = '';
 			
 			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) && $this->args[ 'show_modified' ] == true ) {
-				$time_string = '<' . $this->args['published_container'] . ' ' . $published_container_id . ' ' . $published_container_class  . ' ' . $published_container_attr . '>' .
+				$string = '<' . $this->args['published_container'] . ' ' . $published_container_id . ' ' . $published_container_class  . ' ' . $published_container_attr . '>' .
 									esc_html__( $published_text, FCWP_TAXDOMAIN ) . $this->is_linked_published() . '
 								</' . $this->args['published_container'] . '>
 								<' . $this->args['modified_container'] . ' ' . $modified_container_id . ' ' . $modified_container_class  . ' ' . $modified_container_attr . '>' .
 									esc_html__( $modified_text, FCWP_TAXDOMAIN ) . $this->is_linked_modified() . '
 								</' . $this->args['modified_container'] . '>';
 			} else {
-				$time_string = '<' . $this->args['published_container'] . ' ' . $published_container_id . ' ' . $published_container_class  . ' ' . $published_container_attr . '>' .
+				$string = '<' . $this->args['published_container'] . ' ' . $published_container_id . ' ' . $published_container_class  . ' ' . $published_container_attr . '>' .
 									esc_html__( $published_text, FCWP_TAXDOMAIN ) . $this->is_linked_published() . '
 								</' . $this->args['published_container'] . '>';
 			}
-			$time_string = sprintf( $time_string,
+			$string = sprintf( $string,
 				esc_attr( get_the_date( 'c' ) ),
 				esc_html( get_the_date( $this->args['format'] ) ),
 				esc_attr( get_the_modified_date( 'c' ) ),
 				esc_html( get_the_modified_date( $this->args['format'] ) )
 			);
-			echo '<' . $this->args['container'] . ' ' . $container_id . ' ' . $container_class  . ' ' . $container_attr . '>' . $time_string . '</' . $this->args['container'] . '>';
+			echo '<' . $this->args['container'] . ' ' . $container_id . ' ' . $container_class  . ' ' . $container_attr . '>' . $string . '</' . $this->args['container'] . '>';
 		}
 
 		/**
